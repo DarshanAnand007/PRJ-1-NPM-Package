d8a3e377832dac747156a24bbacdf2ad393b27a9
DarshanAnand007 <darshananand004@gmail.com>
2024-05-13 22:46:05 +0530

    ok

---
+++ packages/client.js
@@ -0,0 +1,6 @@
+const zen = require(("zen-v1"))
+
+console.log(zen.sayHello("Darshan"))
+
+console.log(zen.sayWelcome("Darshan"))
+
---
+++ packages/index.js
@@ -0,0 +1,13 @@
+function sayHello(name) {
+    return `Hello ${name}`
+}
+
+ const sayHi = (name) => {
+    return `Hi ${name}`
+}
+
+function sayWelcome(name) {
+    return `Welcome ${name}`
+}
+
+ module.exports = {sayHello, sayHi, sayWelcome}
